250b6fc6d5c6651a2c4762d06395527d4deb451e
allen <allencv@qq.com>
2024-04-29 20:53:00 +0800

    厚度测量-结果分析

--- src/pages/thickness-measurement/components/task-setting.vue
+++ src/pages/thickness-measurement/components/task-setting.vue
@@ -17,7 +17,35 @@
                 </el-select>
             </div>
         </template>
-        <template #result-content></template>
+        <template #result-content>
+            <div class="item">
+                <TaskTitle name="检测结果" />
+                <div class="head row">
+                    <div class="col">测点序号</div>
+                    <div class="col">测点坐标</div>
+                    <div class="col">厚度尺寸</div>
+                    <div class="col">人工修正</div>
+                </div>
+                <div class="body row" v-for="item in measureResultList">
+                    <div class="col">{{ item.order }}</div>
+                    <div class="col">
+                        {{ item.position[0] }}<br />{{ item.position[1] }}
+                    </div>
+                    <div class="col">{{ item.shape }}</div>
+                    <div class="col">{{ item.hand }}</div>
+                </div>
+                <div class="scale">
+                    <div class="row">
+                        <div class="col">比例尺</div>
+                        <div class="col"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col">x</div>
+                        <div class="col"></div>
+                    </div>
+                </div>
+            </div>
+        </template>
     </TaskSetting>
 </template>
 <script setup>
@@ -34,6 +62,21 @@ const versionOptions = ref([
     {
         label: '型号2',
         value: '2',
+    },
+]);
+
+const measureResultList = ref([
+    {
+        order: '1',
+        position: ['x1,y1', 'x2,y2'],
+        shape: '10mm*10mm',
+        hand: '10mm*10mm',
+    },
+    {
+        order: '2',
+        position: ['x1,y1', 'x2,y2'],
+        shape: '10mm*10mm',
+        hand: '10mm*10mm',
     },
 ]);
 </script>
@@ -66,6 +109,59 @@ const versionOptions = ref([
 
             &.is-selected {
                 background-color: #0d1425;
+            }
+        }
+    }
+
+    .row {
+        display: flex;
+
+        .col {
+            padding: 10px;
+            font-weight: 500;
+            font-size: 12px;
+            min-width: 0;
+            flex: 1;
+            text-align: center;
+            color: #fff;
+            background: #040914;
+            border: 1px solid #626262;
+            border-right: none;
+            border-bottom: none;
+            white-space: wrap;
+            word-wrap: break-word;
+
+            &:first-child {
+                border-left: none;
+            }
+        }
+
+        &:first-child {
+            .col {
+                border-top: none;
+            }
+        }
+    }
+
+    .head {
+        margin-top: 15px;
+        .col {
+            border-top: none;
+            font-size: 16px;
+        }
+    }
+
+    .scale {
+        margin-top: 10px;
+
+        .col {
+            &:nth-child(odd) {
+                flex: none;
+                flex-basis: 68px;
+                white-space: nowrap;
+            }
+            &:nth-child(even) {
+                flex: 1;
             }
         }
     }
