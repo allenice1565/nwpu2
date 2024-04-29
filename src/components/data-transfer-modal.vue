9c668fec1a016ccc2254e0e8b5649adb4bd95857
allen <allencv@qq.com>
2024-04-29 20:27:27 +0800

    数据转换弹框

--- src/components/data-transfer-modal.vue
+++ src/components/data-transfer-modal.vue
@@ -5,12 +5,43 @@
             title="数据转换"
             :before-close="handleClose"
         >
-            <span>This is a message</span>
+            <div class="content">
+                <el-upload
+                    ref="upload1"
+                    class="upload"
+                    v-model:file-list="fileList1"
+                    :limit="1"
+                    :on-exceed="(val) => handleExceed(val, upload1)"
+                    :auto-upload="false"
+                >
+                    <template #trigger>
+                        <div class="input-box">
+                            <span class="placeholder">输入路径</span>
+                            <el-button type="primary">选择文件夹</el-button>
+                        </div>
+                    </template>
+                </el-upload>
+                <el-upload
+                    ref="upload2"
+                    class="upload"
+                    :limit="1"
+                    v-model:file-list="fileList2"
+                    :on-exceed="(val) => handleExceed(val, upload2)"
+                    :auto-upload="false"
+                >
+                    <template #trigger>
+                        <div class="input-box">
+                            <span class="placeholder">输出路径</span>
+                            <el-button type="primary">选择文件夹</el-button>
+                        </div>
+                    </template>
+                </el-upload>
+                <el-progress :percentage="percentage" :show-text="false" />
+            </div>
             <template #footer>
                 <div class="dialog-footer">
-                    <el-button @click="dialogVisible = false">Cancel</el-button>
-                    <el-button type="primary" @click="dialogVisible = false">
-                        Confirm
+                    <el-button type="primary" @click="handleConfirm">
+                        确认
                     </el-button>
                 </div>
             </template>
@@ -18,7 +49,8 @@
     </div>
 </template>
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
+import { genFileId } from 'element-plus';
 
 const emit = defineEmits(['update:dialogVisible']);
 const props = defineProps({
@@ -27,6 +59,18 @@ const props = defineProps({
         default: false,
     },
 });
+const percentage = ref(50);
+const fileList1 = ref();
+const fileList2 = ref();
+const upload1 = ref();
+const upload2 = ref();
+const handleExceed = (files, upload) => {
+    upload?.value?.clearFiles();
+    const file = files?.[0] || {};
+    file.uid = genFileId();
+    upload?.value?.handleStart(file);
+};
+
 const innerVisible = computed({
     get() {
         return props.dialogVisible;
@@ -35,7 +79,12 @@ const innerVisible = computed({
         emit('update:dialogVisible', val);
     },
 });
-const handleClose = () => {};
+const handleClose = () => {
+    innerVisible.value = false;
+};
+const handleConfirm = () => {
+    console.log(fileList1.value, fileList2.value);
+};
 </script>
 <style scoped lang="less">
 .data-transfer-modal {
@@ -52,6 +101,57 @@ const handleClose = () => {};
                 color: #ffffff;
             }
         }
+    }
+}
+
+.content {
+    padding: 0 30px;
+
+    :deep(.upload) {
+        width: 100%;
+        margin-bottom: 30px;
+
+        .el-upload {
+            width: 100%;
+        }
+
+        .input-box {
+            flex: 1;
+            height: 56px;
+            padding-left: 30px;
+            line-height: 56px;
+            background: #232f4e;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .placeholder {
+                font-weight: 500;
+                font-size: 18px;
+                color: #727fa1;
+            }
+
+            .el-button {
+                height: 100%;
+                border-radius: 5px;
+                color: #000;
+            }
+        }
+    }
+}
+
+.dialog-footer {
+    margin: 20px auto 40px;
+    display: flex;
+    justify-content: center;
+
+    :deep(.el-button) {
+        width: 140px;
+        height: 50px;
+        margin: 0 auto;
+        background-color: #687691;
+        border: none;
+        color: #000;
     }
 }
 </style>
