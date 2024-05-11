7e7c7891f3645376f1102e5acfe2e725be0c2c1a
allen <allencv@qq.com>
2024-05-12 00:06:39 +0800

    缺陷检测-检测结果表格

--- src/pages/defect-detection/model-inference/components/task-setting.vue
+++ src/pages/defect-detection/model-inference/components/task-setting.vue
@@ -34,22 +34,36 @@
             </div>
         </template>
         <template #result-content>
-            <div class="item">
+            <div class="item table-list">
                 <TaskTitle name="检测结果" />
-                <div class="head row">
-                    <div class="col">测点序号</div>
-                    <div class="col">测点坐标</div>
-                    <div class="col">厚度尺寸</div>
-                    <div class="col">人工修正</div>
-                </div>
-                <div class="body row" v-for="item in measureResultList">
-                    <div class="col">{{ item.order }}</div>
-                    <div class="col">
-                        {{ item.position[0] }}<br />{{ item.position[1] }}
+                <div ref="resultRef" class="result-list">
+                    <div class="head row">
+                        <div class="col">测点序号</div>
+                        <div class="col">测点坐标</div>
+                        <div class="col">厚度尺寸</div>
+                        <div class="col">人工修正</div>
+                        <div class="col"></div>
                     </div>
-                    <div class="col">{{ item.shape }}</div>
-                    <div class="col">{{ item.hand }}</div>
+                    <div class="body row" v-for="item in dataList">
+                        <div class="col">{{ item.order }}</div>
+                        <div class="col">
+                            {{ item.position[0] }}<br />{{ item.position[1] }}
+                        </div>
+                        <div class="col">{{ item.shape }}</div>
+                        <div class="col">{{ item.hand }}</div>
+                        <div class="col">
+                            <el-icon
+                                color="#fff"
+                                class="delete"
+                                @click="handleDelete(index)"
+                                ><Delete
+                            /></el-icon>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="add" @click="handleAdd">
+                <el-icon color="#68d6e5"><Plus /></el-icon>
             </div>
             <div class="item">
                 <TaskTitle name="推理结果" />
@@ -70,7 +84,7 @@
     </TaskSetting>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 import TaskSetting from '@components/task-setting-template/index.vue';
 import TaskTitle from '@components/task-setting-template/task-title.vue';
 
@@ -97,22 +111,47 @@ const confidenceOptions = ref([
     },
 ]);
 
-const measureResultList = ref([
+const dataList = ref([
     {
-        order: '1',
+        order: 1,
         position: ['x1,y1', 'x2,y2'],
         shape: '10mm*10mm',
         hand: '10mm*10mm',
     },
     {
-        order: '2',
+        order: 2,
         position: ['x1,y1', 'x2,y2'],
         shape: '10mm*10mm',
         hand: '10mm*10mm',
     },
 ]);
+
+const handleDelete = (index) => {
+    dataList.value.splice(index, 1);
+};
+
+const resultRef = ref();
+const handleAdd = async () => {
+    dataList.value.push({
+        order: dataList.value.at(-1)?.order + 1 || 1,
+        position: ['x1,y1', 'x2,y2'],
+        shape: '10mm*10mm',
+        hand: '10mm*10mm',
+    });
+    nextTick(() => {
+        resultRef.value.scrollTo(0, resultRef.value.scrollHeight);
+    });
+};
 </script>
 <style scoped lang="less">
+.result-list {
+    max-height: 260px;
+    overflow-y: auto;
+    margin-top: 15px;
+}
+.table-list {
+    flex: 1;
+}
 .item {
     display: flex;
     flex-direction: column;
@@ -170,6 +209,10 @@ const measureResultList = ref([
             white-space: wrap;
             word-wrap: break-word;
 
+            .delete {
+                cursor: pointer;
+            }
+
             &:first-child {
                 border-left: none;
             }
@@ -186,7 +229,7 @@ const measureResultList = ref([
         margin-top: 15px;
         .col {
             border-top: none;
-            font-size: 16px;
+            font-size: 14px;
         }
     }
 
@@ -225,5 +268,18 @@ const measureResultList = ref([
             height: 40px;
         }
     }
+}
+.add {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 44px;
+    min-height: 44px;
+    border-radius: 3px;
+    border: 1px dashed #68d6e5;
+    cursor: pointer;
+    background: #0d1425;
+    margin-top: 15px;
 }
 </style>
--- src/pages/defect-detection/result-display/components/task-setting.vue
+++ src/pages/defect-detection/result-display/components/task-setting.vue
@@ -2,22 +2,36 @@
     <TaskSetting class="task-setting" fixedTab="result">
         <template #task-content> </template>
         <template #result-content>
-            <div class="item">
+            <div class="item table-list">
                 <TaskTitle name="检测结果" />
-                <div class="head row">
-                    <div class="col">测点序号</div>
-                    <div class="col">测点坐标</div>
-                    <div class="col">厚度尺寸</div>
-                    <div class="col">人工修正</div>
-                </div>
-                <div class="body row" v-for="item in measureResultList">
-                    <div class="col">{{ item.order }}</div>
-                    <div class="col">
-                        {{ item.position[0] }}<br />{{ item.position[1] }}
+                <div ref="resultRef" class="result-list">
+                    <div class="head row">
+                        <div class="col">测点序号</div>
+                        <div class="col">测点坐标</div>
+                        <div class="col">厚度尺寸</div>
+                        <div class="col">人工修正</div>
+                        <div class="col"></div>
                     </div>
-                    <div class="col">{{ item.shape }}</div>
-                    <div class="col">{{ item.hand }}</div>
+                    <div class="body row" v-for="item in dataList">
+                        <div class="col">{{ item.order }}</div>
+                        <div class="col">
+                            {{ item.position[0] }}<br />{{ item.position[1] }}
+                        </div>
+                        <div class="col">{{ item.shape }}</div>
+                        <div class="col">{{ item.hand }}</div>
+                        <div class="col">
+                            <el-icon
+                                color="#fff"
+                                class="delete"
+                                @click="handleDelete(index)"
+                                ><Delete
+                            /></el-icon>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="add" @click="handleAdd">
+                <el-icon color="#68d6e5"><Plus /></el-icon>
             </div>
             <div class="item">
                 <TaskTitle name="推理结果" />
@@ -42,26 +56,50 @@
     </TaskSetting>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 import TaskSetting from '@components/task-setting-template/index.vue';
 import TaskTitle from '@components/task-setting-template/task-title.vue';
 
-const measureResultList = ref([
+const dataList = ref([
     {
-        order: '1',
+        order: 1,
         position: ['x1,y1', 'x2,y2'],
         shape: '10mm*10mm',
         hand: '10mm*10mm',
     },
     {
-        order: '2',
+        order: 2,
         position: ['x1,y1', 'x2,y2'],
         shape: '10mm*10mm',
         hand: '10mm*10mm',
     },
 ]);
+const handleDelete = (index) => {
+    dataList.value.splice(index, 1);
+};
+
+const resultRef = ref();
+const handleAdd = async () => {
+    dataList.value.push({
+        order: dataList.value.at(-1)?.order + 1 || 1,
+        position: ['x1,y1', 'x2,y2'],
+        shape: '10mm*10mm',
+        hand: '10mm*10mm',
+    });
+    nextTick(() => {
+        resultRef.value.scrollTo(0, resultRef.value.scrollHeight);
+    });
+};
 </script>
 <style scoped lang="less">
+.result-list {
+    max-height: 220px;
+    overflow-y: auto;
+    margin-top: 15px;
+}
+.table-list {
+    flex: 1;
+}
 .item {
     display: flex;
     flex-direction: column;
@@ -92,6 +130,10 @@ const measureResultList = ref([
             white-space: wrap;
             word-wrap: break-word;
 
+            .delete {
+                cursor: pointer;
+            }
+
             &:first-child {
                 border-left: none;
             }
@@ -108,7 +150,7 @@ const measureResultList = ref([
         margin-top: 15px;
         .col {
             border-top: none;
-            font-size: 16px;
+            font-size: 14px;
         }
     }
 
@@ -147,5 +189,18 @@ const measureResultList = ref([
             height: 40px;
         }
     }
+}
+.add {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 44px;
+    min-height: 44px;
+    border-radius: 3px;
+    border: 1px dashed #68d6e5;
+    cursor: pointer;
+    background: #0d1425;
+    margin-top: 15px;
 }
 </style>
