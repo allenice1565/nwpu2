69a93e41ccd57c5ee60b100a6281cccf993eea80
lxc <longxc@go-goal.com>
2024-05-11 09:13:50 +0800

    添加删除按钮

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import 'normalize.css/normalize.css';
 import '@assets/css/tailwind.css';
 import ElementPlus from 'element-plus';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import 'element-plus/dist/index.css';
 import '@assets/css/index.less';
@@ -14,5 +15,7 @@ app.use(router);
 app.use(ElementPlus, {
     locale: zhCn,
 });
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.mount('#app');
--- src/pages/data-annotation/annotation/components/task-setting.vue
+++ src/pages/data-annotation/annotation/components/task-setting.vue
@@ -1,17 +1,21 @@
 <template>
     <TaskSetting class="task-setting">
         <template #task-content>
-            <div class="item">
+            <div class="item ">
                 <TaskTitle name="标注结果" />
-                <div class="head row">
-                    <div class="col">目标类别</div>
-                    <div class="col">中心位置</div>
-                    <div class="col">目标大小</div>
-                </div>
-                <div class="body row" v-for="item in annotationList">
-                    <div class="col">{{ item.type }}</div>
-                    <div class="col">{{ item.position }}</div>
-                    <div class="col">{{ item.shape }}</div>
+                <div class="annotation-result">
+                    <div class="head row">
+                        <div class="col">目标类别</div>
+                        <div class="col">中心位置</div>
+                        <div class="col">目标大小</div>
+                        <div class="col"></div>
+                    </div>
+                    <div class="body row" v-for="(item, index) in annotationList">
+                        <div class="col">{{ item.type }}</div>
+                        <div class="col">{{ item.position }}</div>
+                        <div class="col">{{ item.shape }}</div>
+                        <div class="col"><el-icon color="#fff" class="delete" @click="handleDelete(index)"><Delete /></el-icon></div>
+                    </div>
                 </div>
             </div>
         </template>
@@ -44,9 +48,47 @@ const annotationList = ref([
         position: 'x1,y1',
         shape: 'w*h',
     },
+    {
+        type: 'c1',
+        position: 'x1,y1',
+        shape: 'w*h',
+    },
+    {
+        type: 'c1',
+        position: 'x1,y1',
+        shape: 'w*h',
+    },
+    {
+        type: 'c1',
+        position: 'x1,y1',
+        shape: 'w*h',
+    },
+    {
+        type: 'c1',
+        position: 'x1,y1',
+        shape: 'w*h',
+    },
+    {
+        type: 'c1',
+        position: 'x1,y1',
+        shape: 'w*h',
+    },
+    {
+        type: 'c1',
+        position: 'x1,y1',
+        shape: 'w*h',
+    },
 ]);
+const handleDelete = (index)=>{
+    annotationList.value.splice(index, 1)
+}
 </script>
 <style scoped lang="less">
+.annotation-result{
+    max-height: 300px;
+    overflow-y: auto;
+    margin-top: 15px;
+}
 .item {
     display: flex;
     flex-direction: column;
@@ -68,20 +110,23 @@ const annotationList = ref([
             border-right: none;
             border-bottom: none;
 
+            .delete{
+                cursor: pointer;
+            }
+
             &:first-child {
                 border-left: none;
-            }
-        }
-
-        &:first-child {
-            .col {
-                border-top: none;
             }
         }
     }
 
     .head {
         margin-top: 15px;
+
+        &:nth-child(1){
+            margin-top: 0;
+        }
+
         .col {
             border-top: none;
         }
